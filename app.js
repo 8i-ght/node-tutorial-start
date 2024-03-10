a8e27f6ad487452742623d9589fd75c62055409d
8i-ght <needboosts3545@gmail.com>
2024-03-09 18:11:25 -0600

    github example start

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const http = require('http');
+// npm -- global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-})
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
 
-server.listen(5000);
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default) 
 
-// Video 1: 1:27:16
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
